241d256fd0424865076127c400b4e384b955ecf1
Francesco <alemannof43@gmail.com>
2024-12-23 11:29:24 +0100

    data-fetching

---
+++ data-fetching/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+import { GitHubUsers } from './GitHubUsers'
+
+function App() {
+
+  return (
+    <>
+  <div>
+      <h1>GitHub User Search</h1>
+      <GitHubUsers></GitHubUsers>
+      
+    </div>    </>
+  )
+}
+
+export default App
