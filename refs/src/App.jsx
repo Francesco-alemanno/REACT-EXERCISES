3337ffac2290d31d016405fcf412578b28e991f6
Francesco <alemannof43@gmail.com>
2024-12-23 11:42:49 +0100

    refs

---
+++ refs/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { FocusableInput } from './FocusableInput'
+import MountedMessage from './MountedMessage.jsx'
+
+function App() {
+ì
+  return (
+    <>
+<FocusableInput></FocusableInput>  
+<MountedMessage></MountedMessage>
+  </>
+  )
+}
+
+export default App
