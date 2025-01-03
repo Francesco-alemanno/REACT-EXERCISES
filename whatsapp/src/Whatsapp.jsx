f7f9af2b60ef9bed23a970f9b1060be42f71685f
Francesco <alemannof43@gmail.com>
2025-01-03 10:18:47 +0100

    finish

--- whatsapp/src/Whatsapp.jsx
+++ whatsapp/src/Whatsapp.jsx
@@ -1,45 +1,42 @@
-import { useState } from "react"
-import { useWhatsapp } from "./useWhatsapp"
+import { useState } from "react";
+import { useWhatsapp } from "./useWhatsapp";
 
-export function Whatsapp(){
+export function Whatsapp() {
+  const { error, messaggio, sent, ricevuti, scrivendo, handleChange, handleSent, handleKeyPress } = useWhatsapp();
 
-const {error, messaggio, sent,ricevuti,scrivendo, handleChange, handleSent, handleKeyPress}=useWhatsapp()
+  return (
+    <div className="chat-container">
+      <div className="utente">
+        <h3>Carlo</h3>
+        {scrivendo && <p>{scrivendo}</p>}
+      </div>
 
-console.log(sent)
+      <div className="messaggi">
+        {sent.map((msg, index) => (
+          <div key={index}>
+            <div className="messaggi-inviati">
+              <p className="inviati">{msg}</p>
+            </div>
+            {ricevuti[index] && (
+              <div className="messaggi-ricevuti">
+                <p className="ricevuti">{ricevuti[index]}</p>
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
 
-    return(
-        <div className="chat-container">
-         <div className="utente">
-         <h1>Carlo</h1>
-         {scrivendo && (<p>{scrivendo}</p>)} 
-            </div>   
-        
-        
-        <div className="messaggi-inviati">
-             
-            {sent.map((msg, index) => (
-                <p className="inviati" key={index}>{msg}</p>
-            ))}
+      <input
+        type="text"
+        value={messaggio}
+        onChange={handleChange}
+        placeholder="Scrivi il messaggio"
+        onKeyDown={handleKeyPress}
+      />
+      <button onClick={handleSent}>✔️</button>
 
-            
-
-
-        </div>
-        <div className="messaggi-ricevuti">
-        {sent && (ricevuti.map((msg, index)=>(
-                <p className="ricevuti" key={index}>{msg}</p>
-            )))}
-        </div>
-        <input 
-            type="text" 
-            value={messaggio} 
-            onChange={handleChange} 
-            placeholder="scrivi il messaggio" 
-            onKeyDown={handleKeyPress}
-        />
-        <button onClick={handleSent}>✔️</button>
-        {error && <p style={{ color: 'red' }}>{error}</p>}  {/* Mostra l'errore se presente */}
+      {/* Visualizzazione errori */}
+      {error && <p style={{ color: 'red' }}>{error}</p>}
     </div>
-      
-    )
+  );
 }
